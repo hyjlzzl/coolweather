bc92732c3d7d2b460aa7c9fb4cbc87d82106488e
hyjlzzl <hyjlzzl@163.com>
2017-06-13 11:24:58 +0800

    完成定位功能的实现

--- app/src/main/java/cn/lygtc/coolweather/MainActivity.java
+++ app/src/main/java/cn/lygtc/coolweather/MainActivity.java
@@ -1,13 +1,126 @@
 package cn.lygtc.coolweather;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements BDLocationListener {
+
+    private LocationClient locationClient;
+    private TextView countryView;
+    private TextView provinceView;
+    private TextView cityView;
+    private TextView districtView;
+    private TextView streetView;
+    private TextView latlngView;
+
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        countryView = (TextView)findViewById(R.id.countryView);
+        provinceView = (TextView)findViewById(R.id.provinceView);
+        cityView = (TextView)findViewById(R.id.cityView);
+        districtView = (TextView)findViewById(R.id.districtView);
+        streetView = (TextView)findViewById(R.id.streetView);
+        latlngView = (TextView)findViewById(R.id.latlngView);
+
+        locationClient = new LocationClient(getApplicationContext());
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+        option.setIsNeedAddress(true);
+        locationClient.setLocOption(option);
+        locationClient.registerLocationListener(this);
+        List<String> permissionList = new ArrayList<>();
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)!= PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.READ_PHONE_STATE);
+        }
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if(!permissionList.isEmpty()){
+            String[] permissions = permissionList.toArray(new String[permissionList.size()]);
+            ActivityCompat.requestPermissions(this,permissions,1);
+        }else{
+            requestLocation();
+        }
+
+        handler = new Handler(){
+            public void handleMessage(Message message){
+                Bundle location = message.getData();
+                countryView.setText("国别：" + location.getString("country"));
+                provinceView.setText("省份：" + location.getString("province"));
+                cityView.setText("城市：" + location.getString("city"));
+                districtView.setText("区县：" + location.getString("district"));
+                streetView.setText("街道：" + location.getString("street"));
+                latlngView.setText("坐标：" + location.getString("latlng"));
+            }
+        };
+
+
+    }
+
+    private void requestLocation() {
+        locationClient.start();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode==1){
+            if(grantResults.length>0){
+                for(int result:grantResults){
+                    if(result!=PackageManager.PERMISSION_GRANTED){
+                        Toast.makeText(this,"必须同意所有权限才能使用本APP",Toast.LENGTH_LONG).show();
+                        finish();
+                        return;
+                    }
+                }
+                requestLocation();
+            }
+        }
+    }
+
+    @Override
+    public void onReceiveLocation(BDLocation bdLocation) {
+        if(bdLocation.getLocType()==BDLocation.TypeGpsLocation||bdLocation.getLocType()==BDLocation.TypeNetWorkLocation){
+            Bundle data = new Bundle();
+            data.putString("country", bdLocation.getCountry());
+            data.putString("province", bdLocation.getProvince());
+            data.putString("city", bdLocation.getCity());
+            data.putString("district", bdLocation.getDistrict());
+            data.putString("street", bdLocation.getStreet());
+            data.putString("latlng","(" + bdLocation.getLatitude() + "," + bdLocation.getLongitude() + ")");
+            Message message = new Message();
+            message.setData(data);
+            handler.sendMessage(message);
+        }
+    }
+
+    @Override
+    public void onConnectHotSpotMessage(String s, int i) {
+
     }
 }
